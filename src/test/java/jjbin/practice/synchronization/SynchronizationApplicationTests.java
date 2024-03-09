70cde4ecefcfe4dfcf61caa3d25b8564ce8c40db
JJBINY <sisoya0424@gmail.com>
2024-03-09 15:15:31 +0900

    Project Init

---
+++ src/test/java/jjbin/practice/synchronization/SynchronizationApplicationTests.java
@@ -0,0 +1,13 @@
+package jjbin.practice.synchronization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
